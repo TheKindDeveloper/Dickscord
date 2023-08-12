da9766773c0f50363563eaea9112f5e574f4048f
TheKindDeveloper <129861526+TheKindDeveloper@users.noreply.github.com>
2023-08-12 17:42:32 +0200

    Update __init__.py

--- Dickscord/__init__.py
+++ Dickscord/__init__.py
@@ -2,7 +2,7 @@ __title__ = "Dickscord"
 __author__ = "KADIUM"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023 KADIUM"
-__version__ = "2.0"
+__version__ = "2.1"
 
 from .imports import *
 from .Style import *
